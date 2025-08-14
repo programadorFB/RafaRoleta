c46c06b7ad228b2990e44d8f187bec2466293a7d
programadorFB(Roger) <devfuzabalta@gmail.com>
2025-08-14 12:28:57 -0300

    segundo commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'assets/[name]-[hash].css'
+          }
+          return 'assets/[name]-[hash][extname]'
+        }
+      }
+    }
+  },
+  server: {
+    headers: {
+      'Content-Type': 'text/css; charset=utf-8'
+    }
+  }
 })
